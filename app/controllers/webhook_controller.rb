6ac6131f1c7af9eede9e0dae31be30f8c9bbfb5b
mihhus <gi33ti51@gmail.com>
2020-01-28 14:05:21 +0900

    add push-menu

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -11,6 +11,15 @@ class WebhookController < ApplicationController
   end
 
   def callback
+    menu = [
+      "腹筋20回×3セット！",
+      "スクワット100回×1セット！",
+      "腕立て伏せ30回×3セット！",
+      "ランニング！雨が降っていれば、休み^^",
+      "背筋30回×2セット！",
+      "まあ、たまには休んでもいいだろう。",
+    ]
+
     body = request.body.read
 
     signature = request.env['HTTP_X_LINE_SIGNATURE']
@@ -20,13 +29,19 @@ class WebhookController < ApplicationController
 
     events = client.parse_events_from(body)
     events.each { |event|
+      if event.message['text'].include("メニュー")
+        response = menu.sample
+      else
+        response = event.message['text']
+      end  
+
       case event
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
           message = {
             type: 'text',
-            text: event.message['text']
+            text: response
           }
           client.reply_message(event['replyToken'], message)
         when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
