a3dedf9b5e425bb768057a54f691ecaab620bc3e
kageyama0 <kageyamaforwork@gmail.com>
2020-01-29 13:47:17 +0900

    wip

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -18,11 +18,14 @@ class WebhookController < ApplicationController
       head 470
     end
 
-    events = client.parse_events_from(body)
-    events.each { |event|   
-      case event
+    if response1.defiend?
       response1 = ""
       response2 = ""
+    end  
+
+    events = client.parse_events_from(body)
+    events.each { |event|
+      case event
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
