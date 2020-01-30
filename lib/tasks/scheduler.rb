e6124fccc6da57489f20c176dd62878a4d8a5be5
kageyama0 <kageyamaforwork@gmail.com>
2020-01-30 15:34:26 +0900

    add env of group-id && heroku scheduler

---
+++ lib/tasks/scheduler.rb
@@ -0,0 +1,14 @@
+desc "This task is called by the Heroku scheduler add-on"
+task :update_feed => :environment do
+  puts "Sending penalty message to group..."
+  latest_training = Training.order('created_at DESC').first
+  penalty_messages = [
+    "筋トレ失敗…先着１名様に昼食おごります…",
+    "筋トレ失敗…先着３名様に飲み物おごります…",
+    "筋トレ失敗…でもまあ今日はおごらなくてもいいかな、うん"
+  ]
+  if not latest_training.done?
+    client.push_message(ENV['LINE_GROUP_ID'],message)
+  end
+  puts "Sending a message is completed."
+end
