7f8f3157bb1617632d29eaa2d12ad0bb85944004
kageyama0 <kageyamaforwork@gmail.com>
2020-01-30 11:12:05 +0900

    add method && define received_msg && rename column_name

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -5,10 +5,8 @@ class WebhookController < ApplicationController
 
   def client
     @client ||= Line::Bot::Client.new { |config|
-      # config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      # config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-      config.channel_secret = "2250bc69bac8865396abbe8b6086632e"
-      config.channel_token = "huLMDPpMa+A/GT0/WJZNA3lZvDjOBhYjIxSRzehEmiTZm74esUoRzg0R+ug/A1C7NwuGTpJpOozlribUhYXv40LjEPRw1+Vd23qwADllzWyhezcTtJ7kGADZcbLz4WUvMuxS8wDediB1bLm8GFv36QdB04t89/1O/w1cDnyilFU="
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
   end
 
@@ -43,32 +41,31 @@ class WebhookController < ApplicationController
         case event.type
         when Line::Bot::Event::MessageType::Text
           received_msg = event.message['text']
-
           #最新の筋トレ記録を取得する
-          latest_training = Training.order('created_at DESC').first
-          latest_training_date = Date.parse(String(latest_training.created_at).split()[0])
+          @latest_training = Training.order('created_at DESC').first
+          @latest_training_date = Date.parse(String(@latest_training.created_at).split()[0])
 
-          today = Date.today
+          @today = Date.today
 
           muscle_training_menu = [
             "腹筋20回×3セット！",
             "腕立て伏せ30回×3セット！",
             "腹筋&腕立て伏せ20回ずつ×3セット",
-            "ランニング！雨が降っていれば休み。",
+            "ランニング！雨が降っていれば、休み。",
             "スクワット100回×1セット！",
             "背筋30回×2セット！",
             "まあ、たまには休んでもいいだろう。"
           ]
 
           #まだ今日のメニューをもらっていない場合
-          if latest_training_date < today 
+          if @latest_training_date < @today 
             #「メニュー」と送ってきた場合、今日のメニューをランダムに作成
-            if message_for_menu?(received_msg)
+            if event.message['text'] == "メニュー"
               response_for_menu = muscle_training_menu.sample
-              training = Training.create(menu:response_for_menu)
+              @training = Training.create(menu:response_for_menu)
             
             #「やった」or「done」と送ってきた場合
-            elsif message_for_done?(received_msg)
+            elsif event.message['text'] == "やった" or event.message['text'] == "done"
               response_for_menu = "先にメニューを選んでください！"
             end    
 
@@ -78,12 +75,12 @@ class WebhookController < ApplicationController
             #「メニュー」と送ってきた場合
             if message_for_menu?(received_msg)
               response_for_menu = muscle_training_menu.sample
-              latest_training.menu = response_for_menu
+              @latest_training.menu = response_for_menu
             
             #「やった」or「done」
             elsif message_for_done?(received_msg)
               response_for_done = "お疲れさまです"
-              latest_training.done = true
+              @latest_training.check = true
             end
 
           end
